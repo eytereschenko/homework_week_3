e56d46087da8d2cb0be4712ced655ad2b5e944e3
eytereschenko <bezstrawnyiVoin@gmail.com>
2018-01-24 22:45:11 +0200

    Week3...

--- src/part_1/Task2.java
+++ src/part_1/Task2.java
@@ -1,4 +1,5 @@
 package part_1;
+import utils.*;
 /*
 2. Ввести предложение с консоли, найти самое короткое и самое длинное слово.
     Вывести найденные слова и их длину.
@@ -6,8 +7,8 @@ package part_1;
 public class Task2 {
     public static void main(String[] args) {
         String arr[] = utils.StringUtils.scanStringAndCreateStringArray();
-        String max = utils.StringUtils.findMaxLengthWordInArra(arr);
-        String min = utils.StringUtils.findMinLengthWordInArra(arr);
+        String max = StringUtils.findMaxLengthWordInArra(arr);
+        String min = StringUtils.findMinLengthWordInArra(arr);
         System.out.printf("The longest word is: %s; its length is %d." +
                 "\nThe smallest word is %s with %d chars.", max, max.length(), min, min.length());
     }
