50f66d22d07462c83e41eb2b53e1a1251f6d60d4
eytereschenko <bezstrawnyiVoin@gmail.com>
2018-01-25 22:30:07 +0200

    Week3...

--- src/part_2/Task3_1.java
+++ src/part_2/Task3_1.java
@@ -1,15 +1,24 @@
 package part_2;
 import utils.*;
+/*
 
+3. Матрицы генерировать случайными числами и размер матрицы вводит юзер.
+
+	3.1. Заменить все числа, которые делятся на 5 без остачи в двухмерной матрице на 8
+ */
 import java.util.Scanner;
 
 public class Task3_1 {
     public static void main(String[] args) {
         System.out.println("Enter matrix borders: ");
+        int replace = 8;
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
         int[][] array = IntUtils.createRandomIntMatrix(a, b);
         IntUtils.printIntMatrix(array);
+        System.out.println("Now your replaced matrix is:");
+        int[][] res = IntUtils.replaceSomeValues(array, replace);
+        IntUtils.printIntMatrix(res);
     }
 }
--- src/utils/IntUtils.java
+++ src/utils/IntUtils.java
@@ -17,12 +17,27 @@ public class IntUtils {
     }
 
     public static void printIntMatrix(int[][] array) {
-        if (array == null) System.out.println("Enter valid data");
+        if (array == null) {
+            System.out.println("Enter valid data");
+            return;
+        }
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 System.out.printf("%5d",array[i][j]);
             }
             System.out.println();
         }
+    }
+
+    public static int[][] replaceSomeValues(int[][] array, int replace) {
+        if (array == null) return null;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if(array[i][j]%5==0){
+                    array[i][j]=replace;
+                }
+            }
+        }
+        return array;
     }
 }
